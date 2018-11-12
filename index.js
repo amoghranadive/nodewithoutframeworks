af291dc1c3e97fd828c0a11d113cbe8ccca71528
Ranadive <Amogh.Ranadive@thomsonreuters.com>
2018-11-12 14:04:56 -0500

    parsing url

--- index.js
+++ index.js
@@ -4,9 +4,22 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
-var server = http.createServer(function(req, res){
-     res.end("Hello World \n");
+var server = http.createServer(function(req, res) {
+
+    // Get the URL and parse it
+    var parsedURL = url.parse(req.url, true);
+
+    // Get the path
+    var path = parsedURL.pathname
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // send the response
+    res.end("hello World\n");
+
+    // log the request path
+    console.log(`Request received on ${trimmedPath}`);    
 })
 
 // start the server
