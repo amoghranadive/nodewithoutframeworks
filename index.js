84018e0640966575cf931a0d6f49801c83324b07
Ranadive <Amogh.Ranadive@thomsonreuters.com>
2018-11-12 14:58:07 -0500

    parse query string, method, headers

--- index.js
+++ index.js
@@ -12,14 +12,25 @@ var server = http.createServer(function(req, res) {
     var parsedURL = url.parse(req.url, true);
 
     // Get the path
-    var path = parsedURL.pathname
+    var path = parsedURL.pathname;
     var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    // get the query string as an object
+    var queryStringObject = parsedURL.query;
+
+    // get the HTTP method
+    var method = req.method.toUpperCase();
+
+    // get the headers
+    var headers = req.headers;
 
     // send the response
     res.end("hello World\n");
 
     // log the request path
-    console.log(`Request received on ${trimmedPath}`);    
+    console.log(`Request received on ${trimmedPath} with method: ${method}`);
+    console.log('Query string parameters: %j', queryStringObject);    
+    console.log("Headers: %j", headers);
 })
 
 // start the server
